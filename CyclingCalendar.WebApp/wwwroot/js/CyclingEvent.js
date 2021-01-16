68ff13282fb7c211ba1448711c254526b20f1abc
Bruno Fernandes <brunof.89@gmail.com>
2021-01-16 18:39:03 +0000

    Edit Event

--- CyclingCalendar.WebApp/wwwroot/js/CyclingEvent.js
+++ CyclingCalendar.WebApp/wwwroot/js/CyclingEvent.js
@@ -1,4 +1,6 @@
-﻿$(document).ready(function () {
+﻿var events = [];
+
+$(document).ready(function () {
     console.log("App Loaded!");
     getEvents();
 });
@@ -7,36 +9,56 @@ function getEvents() {
     $('#table-events-body').html('');
     $.ajax({
         url: "https://localhost:5001/api/Calendar", success: function (result) {
+            events = result;
             result.forEach((r,idx )=> {
                 $('#table-events-body')
                     .append('<tr><td>' +
                         (idx + 1) + '</td><td>' +
                         r.description + '</td><td>' +
-                        r.date + '</td><td><i onclick="openEditModal(' + r.id + ')" class="icon-pencil"></i></td><td><i onclick="deleteEvent(\'' + r.id + '\')" class="icon-trash"></i></td><tr>')
+                    r.date + '</td><td><button class="btn btn-warning" onclick="openEditModal(\'' + r.id + '\')">Edit</button></td><td><button onclick="deleteEvent(\'' + r.id + '\')" class="btn btn-danger">Delete</button></td><tr>')
             });
         }
     });
 }
 
+function openEditModal(id) {
+    debugger;
+    var data = events.filter(function (e) { return e.id === id })[0];
+
+    $('#newEventName').val(data.description);
+    $('#eventId').val(data.id);
+    $('#newEventDate').val(new Date(data.date).toLocaleDateString());
+
+
+    $('#eventModal').modal('toggle');
+    $('#eventModal').modal('show');
+}
+
+function closeModal() {
+    $('#eventModal').modal('hide');
+}
+
 function addEvent() {
     var data ={
-        "id": generateUUID(),
+        "id": $('#eventId').val() ? $('#eventId').val(): generateUUID(),
         "date": $('#newEventDate').val(),
         "description": $('#newEventName').val(),
         "active": true
     };
 
     $.ajax({
-        type: 'POST',
+        type: $('#eventId').val() ? 'PUT' : 'POST',
         dataType: 'json',
         contentType: 'application/json',
         url: "https://localhost:5001/api/Calendar",
         data: JSON.stringify(data),
         success: function (result) {
             getEvents();
+            closeModal();
         },
         error: function () {
             getEvents();
+            closeModal();
         }
     });
 }
@@ -50,9 +72,11 @@ function deleteEvent(id) {
         data: null,
         success: function (result) {
             getEvents();
+            closeModal();
         },
         error: function () {
             getEvents();
+            closeModal();
         }
     });
 }
